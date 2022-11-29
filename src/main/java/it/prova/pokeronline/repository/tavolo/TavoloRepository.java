7af9f08ed6df04ce904fac9e41ee308c90858393
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-29 16:28:30 +0100

    metodi eager di tavolo nel repository

--- src/main/java/it/prova/pokeronline/repository/tavolo/TavoloRepository.java
+++ src/main/java/it/prova/pokeronline/repository/tavolo/TavoloRepository.java
@@ -1,9 +1,17 @@
 package it.prova.pokeronline.repository.tavolo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import it.prova.pokeronline.model.Tavolo;
 
 public interface TavoloRepository extends CrudRepository<Tavolo, Long> {
 
+	@Query("from Tavolo t join fetch t.utenteCreazione left join fetch t.giocatori where t.id = ?1")
+	Tavolo findSingleTavoloEager(Long id);
+	
+	@Query("from Tavolo t join fetch t.utenteCreazione left join fetch t.giocatori")
+	List<Tavolo> findAllTavoloEager();
 }
