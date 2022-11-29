82355145f66dc7404f2fe4491e203daeaa5bd5bd
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-29 12:14:00 +0100

    inizializzazione di 3 utenti nell'application

--- src/main/java/it/prova/pokeronline/PokeronlineApplication.java
+++ src/main/java/it/prova/pokeronline/PokeronlineApplication.java
@@ -1,13 +1,67 @@
 package it.prova.pokeronline;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PokeronlineApplication {
+import it.prova.pokeronline.model.Ruolo;
+import it.prova.pokeronline.model.Utente;
+import it.prova.pokeronline.service.ruolo.RuoloService;
+import it.prova.pokeronline.service.utente.UtenteService;
 
+@SpringBootApplication
+public class PokeronlineApplication implements CommandLineRunner {
+
+	@Autowired
+	private RuoloService ruoloServiceInstance;
+	
+	@Autowired
+	private UtenteService utenteServiceInstance;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PokeronlineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Administrator", Ruolo.ROLE_ADMIN));
+		}
+
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Classic Player", Ruolo.ROLE_PLAYER) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Classic Player", Ruolo.ROLE_PLAYER));
+		}
+		
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Special Player", Ruolo.ROLE_SPECIAL_PLAYER) == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Special Player", Ruolo.ROLE_SPECIAL_PLAYER));
+		}
+		
+		if (utenteServiceInstance.findByUsername("admin") == null) {
+			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
+			admin.getRuoli().add(ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", Ruolo.ROLE_ADMIN));
+			utenteServiceInstance.inserisciNuovo(admin);
+			utenteServiceInstance.changeUserAbilitation(admin.getId());
+		}
+
+		if (utenteServiceInstance.findByUsername("player") == null) {
+			Utente playerUser = new Utente("player", "player", "Antonio", "Verdi", new Date());
+			playerUser.getRuoli()
+					.add(ruoloServiceInstance.cercaPerDescrizioneECodice("Classic User", Ruolo.ROLE_PLAYER));
+			utenteServiceInstance.inserisciNuovo(playerUser);
+			utenteServiceInstance.changeUserAbilitation(playerUser.getId());
+		}
+
+		if (utenteServiceInstance.findByUsername("specialPlayer") == null) {
+			Utente specialPlayerUser = new Utente("specialPlayer", "specialPlayer", "Antonioo", "Verdii", new Date());
+			specialPlayerUser.getRuoli()
+					.add(ruoloServiceInstance.cercaPerDescrizioneECodice("Classic User", Ruolo.ROLE_SPECIAL_PLAYER));
+			utenteServiceInstance.inserisciNuovo(specialPlayerUser);
+			utenteServiceInstance.changeUserAbilitation(specialPlayerUser.getId());
+		}
 	}
 
 }
