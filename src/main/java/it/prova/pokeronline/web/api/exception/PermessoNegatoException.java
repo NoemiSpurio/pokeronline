ab8e9244a0b090b2e1b9db91cc4bdfa28e7f82ce
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-30 10:12:00 +0100

    aggiunte custom exceptions

---
+++ src/main/java/it/prova/pokeronline/web/api/exception/PermessoNegatoException.java
@@ -0,0 +1,10 @@
+package it.prova.pokeronline.web.api.exception;
+
+public class PermessoNegatoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public PermessoNegatoException(String message) {
+		super(message);
+	}
+}
