cf9730e87cada4d0f0e666773671dbcbf88e7b70
NoemiSpurio <noemi.spurio@gmail.com>
2022-11-30 10:11:42 +0100

    creazione metodo in UtenteDTO

--- src/main/java/it/prova/pokeronline/dto/UtenteDTO.java
+++ src/main/java/it/prova/pokeronline/dto/UtenteDTO.java
@@ -2,6 +2,7 @@ package it.prova.pokeronline.dto;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.constraints.NotBlank;
@@ -165,6 +166,12 @@ public class UtenteDTO {
 					.toArray(new Long[] {});
 
 		return result;
+	}
+
+	public static List<UtenteDTO> createUtenteDTOListFromModelList(List<Utente> modelListInput) {
+		return modelListInput.stream().map(utenteEntity -> {
+			return UtenteDTO.buildUtenteDTOFromModel(utenteEntity);
+		}).collect(Collectors.toList());
 	}
 
 }
